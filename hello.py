fb1cc94e05787c3e2939a17796267418382920c2
BhandariLakshay <lbhandari@wisc.edu>
2024-06-02 18:29:23 -0500

    1st assignment commits

--- hello.py
+++ hello.py
@@ -1,2 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 18:25:02 2024
+
+@author: luckm
+"""
+
 def hello_world():
-	return "Hello"
+    return "Hello World!"
+def hello_world_n(N):
+    return " ".join(["Hello World!"] * N)
